bc3265391f8ec1bd2ae08aa6664ec54e5eacf21f
Robles <cristian.robles@capgemini.com>
2021-07-20 16:26:23 -0500

    Added new annotations to files and made code in appconfig obselete

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,26 +2,34 @@ import com.potatogod123.repository.HibernateSpeakerRepositoryImpl;
 import com.potatogod123.repository.SpeakerRepository;
 import com.potatogod123.service.SpeakerService;
 import com.potatogod123.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.potatogod123"})
 public class AppConfig {
 
-    @Bean(name = "speakerService")
-    public SpeakerService getSpeakerService(){
-        //setter injection
-        //        SpeakerServiceImpl service = new SpeakerServiceImpl();
-        //        service.setRepository(getSpeakerRepository());
+//    @Bean(name = "speakerService")
+//    @Scope(value = BeanDefinition.SCOPE_SINGLETON)// for single instance
+//    //SCOPE_PROTOTYPE is for multiple instances
+//    //@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
+//    public SpeakerService getSpeakerService(){
+//        //setter injection
+//        //        SpeakerServiceImpl service = new SpeakerServiceImpl();
+//        //        service.setRepository(getSpeakerRepository());
+//
+//        //Constructor Injection
+//        //SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+//
+//        SpeakerServiceImpl service = new SpeakerServiceImpl();
+//        return service;
+//    }
 
-        //Constructor Injection
-        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
-
-        return service;
-    }
-
-    @Bean(name = "speakerRepository")
-    public SpeakerRepository getSpeakerRepository(){
-        return new HibernateSpeakerRepositoryImpl();
-    }
+//    @Bean(name = "speakerRepository")
+//    public SpeakerRepository getSpeakerRepository(){
+//        return new HibernateSpeakerRepositoryImpl();
+//    }
 }
