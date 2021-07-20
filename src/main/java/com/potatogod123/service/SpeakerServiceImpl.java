e62bada021d8d41d6efad2bd14ee85942ed3f113
Robles <cristian.robles@capgemini.com>
2021-07-20 15:50:58 -0500

    Intergrated spring framework with old code refactored into beans and injected into appconfig

--- src/main/java/com/potatogod123/service/SpeakerServiceImpl.java
+++ src/main/java/com/potatogod123/service/SpeakerServiceImpl.java
@@ -8,10 +8,19 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl (SpeakerRepository speakerRepository){
+        this.repository= speakerRepository;
+    }
 
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
+    }
+
+
+    public void setRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
